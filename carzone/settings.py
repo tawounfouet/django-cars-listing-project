684391588e33ad6893c9619b0e4b54293ab963e4
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2022-01-03 19:13:14 +0100

    created pages and configured navigation links

--- carzone/settings.py
+++ carzone/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS =[
+    BASE_DIR / 'carzone/static',
+]
