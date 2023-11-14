d35eb30ac74d094191e79d6f64ea2c2705a0b269
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-11-14 18:34:40 +0100

    finishing up search form fonctionnality

--- cars/views.py
+++ cars/views.py
@@ -31,8 +31,55 @@ def car_detail(request, id):
     }
     return render(request, 'cars/car_detail.html', data)
 
+# def search(request):
+#     cars = Car.objects.order_by('-created_date')
+
+#     if 'keyword' in request.GET:
+#         keyword = request.GET['keyword']
+#         if keyword:
+#             cars = cars.filter(description__icontains=keyword)
+
+#     if 'model' in request.GET:
+#         model = request.GET['model']
+#         if model:
+#             cars = cars.filter(model_iexact=model)
+
+#     if 'city' in request.GET:
+#         city = request.GET['city']
+#         if city:
+#             cars = cars.filter(model_iexact=city)
+
+#     if 'year' in request.GET:
+#         year = request.GET['year']
+#         if year:
+#             cars = cars.filter(model_iexact=year)
+
+#     if 'body_style' in request.GET:
+#         body_style = request.GET['body_style']
+#         if body_style:
+#             cars = cars.filter(model_iexact=body_style)
+
+#     if 'min_price' in request.GET:
+#         min_price = request.GET['min_price']
+#         max_price = request.GET['max_price']
+#         if max_price:
+#             cars.filter(price__gte=min_price, price__lte=max_price)
+
+
+#     data = {
+#         'cars': cars,
+#     }
+#     return render(request, 'cars/search.html', data)
+
+
 def search(request):
     cars = Car.objects.order_by('-created_date')
+
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    transmission_search = Car.objects.values_list('transmission', flat=True).distinct()
 
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
@@ -42,31 +89,35 @@ def search(request):
     if 'model' in request.GET:
         model = request.GET['model']
         if model:
-            cars = cars.filter(model_iexact=model)
+            cars = cars.filter(model__iexact=model)
 
     if 'city' in request.GET:
-        city = request.GET['model']
+        city = request.GET['city']
         if city:
-            cars = cars.filter(model_iexact=city)
+            cars = cars.filter(city__iexact=city)
 
     if 'year' in request.GET:
-        year = request.GET['model']
+        year = request.GET['year']
         if year:
-            cars = cars.filter(model_iexact=year)
+            cars = cars.filter(year__iexact=year)
 
     if 'body_style' in request.GET:
         body_style = request.GET['body_style']
         if body_style:
-            cars = cars.filter(model_iexact=body_style)
+            cars = cars.filter(body_style__iexact=body_style)
 
     if 'min_price' in request.GET:
         min_price = request.GET['min_price']
         max_price = request.GET['max_price']
         if max_price:
-            cars.filter(price__gte=min_price, price__lte=max_price)
-
+            cars = cars.filter(price__gte=min_price, price__lte=max_price)
 
     data = {
         'cars': cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+        'transmission_search': transmission_search,
     }
     return render(request, 'cars/search.html', data)
